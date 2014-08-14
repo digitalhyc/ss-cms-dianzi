67bf265330275ef7d64409152d80bab8259dfc9f
Junzki <sswgmxain@gmail.com>
2014-08-14 17:04:23 +0800

    Init Commit

---
+++ ss_cms_dianzi/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for ss_cms_dianzi project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ss_cms_dianzi.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()

2faa00b962aeb2588b32d95247fcea3d0b0e5d8f
Junzki <sswgmxain@gmail.com>
2014-08-25 20:16:15 +0800

    Modified settings

--- ss_cms_dianzi/settings.py
+++ ss_cms_dianzi/settings.py
@@ -62,7 +62,7 @@ WSGI_APPLICATION = 'ss_cms_dianzi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '_db/dianzi.db'),
         'USER': '',
         'PASSWORD': '',
         'HOST': '',
